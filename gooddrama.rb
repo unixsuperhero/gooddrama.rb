25661bcfb7a81ca3ab6d66766d157ae89041abb1
Joshua Toyota <jearsh@gmail.com>
2014-02-14 06:22:13 -0500

    handles optional mirror param: $> gooddrama.rb $episode_url [$mirror_number]

--- gooddrama.rb
+++ gooddrama.rb
@@ -6,11 +6,12 @@ require 'pp'
 
 class GoodDrama
   attr_accessor :uri, :parts, :embeds, :files
-  attr_accessor :response, :body, :part
+  attr_accessor :response, :body, :part, :mirror
 
-  def initialize(uri, part=1)
+  def initialize(uri, part=1, mirror=1)
     @uri = URI(uri)
     @part = part
+    @mirror = mirror.to_i
     @embeds = []
     @files = []
   end
@@ -25,11 +26,11 @@ class GoodDrama
   end
 
   def parts_regex
-    /href=['"]([^"']*\/1-(\d\d*))["'][^>]*>part\s*\d\d*/i
+    /href=['"]([^"']*\/#{mirror}-(\d\d*))["'][^>]*>part\s*\d\d*/i
   end
 
   def parts
-    @parts ||= [[uri,'1']] + body.scan(parts_regex).tap{|p| p.shift }
+    @parts ||= [[uri,'1']] + body.scan(parts_regex)
   end
 
   def embed_regex
@@ -37,7 +38,7 @@ class GoodDrama
   end
 
   def embed
-    @embed ||= body.scan(file_regex).grep(/mp4|flv/i).first
+    @embed ||= body.scan(file_regex).grep(/mp4|flv/i).first(mirror).last
   end
 
   def file_regex
@@ -47,26 +48,26 @@ class GoodDrama
   def file
     uri = URI(embed)
     r = Net::HTTP.get_response(uri)
-    URI.decode r.body.scan(file_regex).first
+    URI.decode r.body.scan(file_regex).first.to_s
   end
 
   def embeds
     parts.map do |url,part|
-      (url == @url) ? embed : GoodDrama.new(url,part).embed
+      (url == @url) ? embed : GoodDrama.new(url,part,mirror).embed
     end
   end
 
   def files
     parts.map do |url,part|
-      (url == @url) ? file : GoodDrama.new(url,part).file
+      (url == @url) ? file : GoodDrama.new(url,part,mirror).file
     end
   end
 end
 
 wgets = []
-GoodDrama.new(ARGV[0]).files.each do |file|
-  wgets.push "wget -cq '#{file}' &"
-  `open '#{file}'` unless !!ARGV[1]
+GoodDrama.new(ARGV[0], 1, ARGV[1] || 1).files.each do |file|
+  wgets.push "wget -cb '#{file}'"
+  # `open '#{file}'` unless !!ARGV[2]
 end
 
 puts wgets
